369614bee5604b4062ccf38cf0bc98d831f2fe11
LentinulaEdode <carmona.a.esther@gmail.com>
2024-02-09 15:00:55 +0100

    starter portfolio

---
+++ src/app/portfolio/page.jsx
@@ -0,0 +1,10 @@
+const PortfolioPage = () => {
+    return (
+        <div>
+        <h1>Portfolio</h1>
+        <p>This is the trabajitos page</p>
+        </div>
+    );
+}
+
+export default PortfolioPage;
