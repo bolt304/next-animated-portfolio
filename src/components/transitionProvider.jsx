60f5c20c3ccba6b24a653c74005c5545d0f5e7ab
LentinulaEdode <carmona.a.esther@gmail.com>
2024-02-19 22:03:27 +0100

    transiciones 20%

--- src/components/transitionProvider.jsx
+++ src/components/transitionProvider.jsx
@@ -1,11 +1,27 @@
 "use client";
-import {AnimatePresence} from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
 import Navbar from './navbar';
+import { motion } from 'framer-motion';
+import { usePathname } from 'next/navigation';
 
 const TransitionProvider = ({children}) => {
+
+    const pathName = usePathname();
+
     return (
-        <AnimatePresence>
-        <div className="w-screen h-screen bg-gradient-to-b from-purple-50 to-pink-200">
+        <AnimatePresence mode='wait'>
+        <div key={pathName} className="w-screen h-screen bg-gradient-to-b from-purple-50 to-pink-200">
+            <motion.div 
+            className="h-screen w-screen fixed bg-pink-300 rounded-b-[100px] z-40"
+            animate={{height:"0vh"}} 
+            exit={{height:"140vh"}}
+            transition={{duration: 0.7, ease: "easeOut"}}
+            />
+            <motion.div 
+            className="h-screen w-screen fixed bg-pink-300 rounded-t-[100px] bottom-0 z-40"
+            initial={{height:"140vh"}} 
+            animate={{height:"0vh", transition: {delay: 0.5}}}
+            />
           <div className="h-24">
             <Navbar />
           </div>
