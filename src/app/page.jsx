f35cff930fd40931ff243ee03d86865a8826ffad
LentinulaEdode <carmona.a.esther@gmail.com>
2024-02-22 16:59:20 +0100

    Botones funcionales para la Homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,8 @@
 "use client";
+import React from "react";
 import Image from "next/image";
 import {motion} from 'framer-motion';
+import Link from 'next/link';
 
 const Homepage = () => {
   return (
@@ -17,15 +19,19 @@ const Homepage = () => {
        <Image src="/knight.png" alt="" fill className="object-contain"/>
       </div>
     {/* Texto */}
-    <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-6 mr-2 ml-4 items-center justify-center">
+    <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 mr-2 ml-4 items-center justify-center">
     {/* Title */}
       <h1 className="text-xl md:text-xl font-semibold">From Historian to Full Stack Developer: My Web Journey</h1>
     {/* Descripción */}
-      <p className="md:text-l">Portfolio dedicado a demostrar mis habilidades técnicas y creativas, mostrando proyectos personales y ejercicios prácticos que reflejan mi dedicación reciente por la programación y mi compromiso con el aprendizaje continuo en el campo del desarrollo web.</p>
+      <p className="md:text-l font-medium">Portfolio dedicado a demostrar mis habilidades técnicas y creativas, mostrando proyectos personales y ejercicios prácticos que reflejan mi dedicación reciente por la programación y mi compromiso con el aprendizaje continuo en el campo del desarrollo web.</p>
     {/* Buttons */}
         <div className="flex gap-4">
-          <button className="p-4 rounded-lg ring-1 ring-pink-300 bg-pink-300 text-black">View my work</button>
-          <button className="p-4 rounded-lg ring-1 ring-pink-300 bg-pink-300 text-black">Contact Me</button>
+          <Link href="/projects">
+          <button className="p-3 rounded-lg ring-5 ring-pink-300 bg-pink-300 text-black font-medium tracking-tighter">View my work</button>
+          </Link>
+          <Link href="/contact">
+          <button className="p-3 rounded-lg ring-5 ring-pink-300 bg-pink-300 text-black font-medium tracking-tighter">Contact Me</button>
+          </Link>
         </div>
       </div>
     </div>
