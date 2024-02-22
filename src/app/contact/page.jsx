ff5599d90bd61c454ebe5a831ef68e31995f938a
LentinulaEdode <carmona.a.esther@gmail.com>
2024-02-22 17:58:10 +0100

    construcción del Contact Form

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -5,11 +5,21 @@ import Image from 'next/image';
 const ContactPage = () => {
     return (
         <motion.div 
-    className="h-full" 
+    className="min-h-80 flex flex-col items-center overflow-hidden" 
     initial={{y: "-100vh"}} 
     animate={{y: "0%" }} 
     transition={{ duration: 0.6 }}
     >
+        <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-40">
+            {/* Text Container */}
+            <div className="h-1/2 lg:h-full lg:w-1/2">
+                <motion.div>
+                    
+                </motion.div>
+            </div>
+            {/* Form Container */}
+            <form className="h-1/2 lg:h-full lg:w-1/2"></form>
+        </div>
         <div className="sm:h-2/3 w-2/4 md:w-2/3 lg:w-full relative overflow-hidden">
           <Image src="/contact.png" alt="" width={400} height={400}/>
         </div>
